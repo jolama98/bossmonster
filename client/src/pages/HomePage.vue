806ebf7e1723847d7f5842a9e1f131174d462d9f
jo <josehehall98@gmail.com>
2024-10-03 14:56:34 -0600

    player card

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,11 +1,32 @@
 <script setup>
-
+import PlayerCard from '@/components/PlayerCard.vue';
 </script>
 
 <template>
-<h1>home page</h1>
+  <div class="container-fluid ">
+    <div class="row">
+      <div class="col-12">
+        <div class="pb-3">
+          <PlayerCard />
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <div class="d-flex justify-content-center">
+          <img class="boss-monster img-fluid border border-4 border-dark rounded-5"
+            src="https://www.investopedia.com/thmb/xXU54x-dkZf9WRNAYqZs6ECcCTg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/single-word-taxes-on-wooden-block-1132754811-f3ef431cc47a4be3a49223b20774845f.jpg"
+            alt="">
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.boss-monster {
+  height: 25dvh;
+}
 </style>
